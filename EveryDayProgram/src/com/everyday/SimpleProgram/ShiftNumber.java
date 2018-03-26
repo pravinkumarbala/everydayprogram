ddb7d62c6465a975ffe9c5f91ea1fac080a7a61d
pravin <pravin.bala@a-cti.com>
2018-03-26 14:21:21 +0530

    Shift number

--- EveryDayProgram/src/com/everyday/SimpleProgram/ShiftNumber.java
+++ EveryDayProgram/src/com/everyday/SimpleProgram/ShiftNumber.java
@@ -3,26 +3,22 @@ package com.everyday.SimpleProgram;
 import java.util.*;
 
 public class ShiftNumber {
-    public static List<Integer> reverseNumber = new LinkedList<>();
-
-    public static void swapNumber(int number){
-        int a = number / 10;
-        int b = number % 10;
-        int result = b * 10 + a;
-        reverseNumber.add(result);
-    }
 
     public static void main(String[] args) {
         System.out.println("Shifting the numbers");
         System.out.println("--------------------");
         System.out.print("Enter the number : ");
         int number = new Scanner(System.in).nextInt();
+        int pow = 1, result = 0;
         while (number > 0){
             int twoNumber = number % 100;
             number /= 100;
-            swapNumber(twoNumber);
+            int a = twoNumber / 10;
+            int b = twoNumber % 10;
+            int shiftNumber = b * 10 + a;
+            result = pow * shiftNumber + result;
+            pow *= 100;
         }
-        Collections.sort(reverseNumber, Collections.reverseOrder());
-        System.out.println("Reverse Number : " + reverseNumber);
+        System.out.println("Reverse Number : " + result);
     }
 }
